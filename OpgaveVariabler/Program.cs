cca37376757e3b250f6379d2266c20f8bcb6fab1
Henrik Breer <henrik_breer@hotmail.com>
2018-08-23 15:42:19 +0200

    Variable

---
+++ OpgaveVariabler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveVariabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 10;
+            tal++;
+            tal--;
+            tal+= 20;
+            Console.WriteLine(tal);
+
+            double Kommatal = 12.3;
+            Console.WriteLine(Kommatal);
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+            Console.ReadKey();
+        }
+    }
+}
--- Variabler/Program.cs
+++ Variabler/Program.cs
@@ -10,8 +10,67 @@ namespace Variabler
     {
         static void Main(string[] args)
         {
-            //  Kommentar test
-            //  Kommentar test 2
+            //tre forskellige variable
+            int _nummer;
+            int Nummer;
+            int nummer;
+            //standard: Start med småt og lave store bogstaver undervejs:
+            int antalMaanederPrAar;  //pascalCasing
+            antalMaanederPrAar = 0;
+            int a, b, c;
+            int d = 0, e = 0, f = 0;
+
+            System.Int32  i = 1; // svarer til int
+            System.Int64 ii = 1; // svarer til long
+            long g = 3000000000;
+            int h = 2000000000;
+
+            Console.WriteLine(g);
+            Console.WriteLine("g = "+ g);
+            Console.WriteLine($"g = {g}"); // template streng
+
+            int y = 10;
+            Console.WriteLine(y.ToString()); // instans medlem
+            Console.WriteLine(int.MaxValue); // statisk medlem
+
+
+            //float q = 0.01; Fejl da xxx.yyyy opfattes som double
+            float q = 0.01F;
+            double w = 0.01;  //standard for xxx.yyyy
+            decimal ww = 0.01M;
+
+
+            double tal = 013130.1231414125;
+            Console.WriteLine(tal.ToString());
+            Console.WriteLine(tal.ToString("N2"));
+            Console.WriteLine(tal.ToString("N3"));
+            Console.WriteLine(tal.ToString("F2"));
+            Console.WriteLine(tal.ToString("C2"));
+            Console.WriteLine(tal.ToString("P2"));
+            
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
