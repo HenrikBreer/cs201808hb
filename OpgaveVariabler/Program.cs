caef56327976f7187fb9d4ec296190939f0b9b45
Henrik Breer <henrik_breer@hotmail.com>
2018-08-23 15:51:03 +0200

    .

--- OpgaveVariabler/Program.cs
+++ OpgaveVariabler/Program.cs
@@ -16,16 +16,25 @@ namespace OpgaveVariabler
             tal+= 20;
             Console.WriteLine(tal);
 
-            double Kommatal = 12.3;
-            Console.WriteLine(Kommatal);
+            double kommatal1 = 12.3;
+            Console.WriteLine(kommatal1);
 
             double kommatal = 12.5;
             kommatal++;
             kommatal--;
             kommatal *= 2;
             Console.WriteLine(kommatal);
-
+            int uU = 2;
             Console.ReadKey();
+
+            bool b1 = false;
+            bool b2 = false;
+
+            if (b1 && b2) { Console.WriteLine("true"); }
+
+
+
+
         }
     }
 }
