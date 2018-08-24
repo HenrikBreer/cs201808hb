16312a9ace42cb7d886319b88e198d9233b90492
Henrik Breer <henrik_breer@hotmail.com>
2018-08-24 15:20:05 +0200

    .

---
+++ Hukommelse/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hukommelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack:
+            int i = 1;
+            int y = 2;
+            i = y; //kopiering af værdier
+            y = 3;
+
+            //
+            Person p; //Pegepind på stack (peger ikke på noget endnu
+            p = new Person(); //peger nu på object på heap hvor alder er 10
+            //Alternativt
+            Person q = new Person() { alder = 12 };
+
+            p = null; //pegepind til object forsvinder. objectet kan herefter slettes, hvis der ikke er andre variable der peger på det
+
+            p = q; //nu peger både p og q på samme object;
+
+            q = null; //nu er det kun p der peger på object;
+
+            // p og q er referencebaserede variable.
+
+            //Struct er værdibaserede, class er referencebaseret
+
+
+
+
+
+
+
+
+        }
+    }
+    class Person
+    {
+        public int alder = 10;
+
+    }
+
+
+}
---
+++ SimpelFejlhåndtering/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpelFejlhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        private static void Run()
+        {
+
+
+            try
+            {
+                Console.WriteLine("Indtast 1. tal");
+                int tal1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Indtast 2. tal");
+                int tal2 = Convert.ToInt32(Console.ReadLine());
+                int res = tal1 + tal2;
+                Console.WriteLine("resultatet er " + res);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Fejl");
+                //ApplicationException aex = new ApplicationException("Det indtastede er ikke et tal.", ex);
+
+                //throw (aex);
+               
+                
+            }
+            
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
